11950825d7ad9aace2b12bf573bdcadabbc7e1aa
Konstantin Denerz <konstantin.denerz@gmail.com>
2024-06-18 19:13:47 +0200

    create demo app with token lib

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {MatButton, MatFabButton, MatIconButton} from '@angular/material/button';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardAvatar,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardSubtitle,
+  MatCardTitle
+} from '@angular/material/card';
+import {MatIcon} from '@angular/material/icon';
+import {MatToolbar} from '@angular/material/toolbar';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'labs-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatIconButton, MatCard, MatCardHeader, MatCardContent, MatCardTitle, MatCardSubtitle, MatCardAvatar, MatCardActions, MatCardImage, MatButton, MatFabButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
